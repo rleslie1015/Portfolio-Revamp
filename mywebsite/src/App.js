fc226021a98fe3ef3ce16ae063d3fcc96f11a0cf
Leslie Rodriguez <rleslie1015@gmail.com>
2020-12-24 23:20:23 -0600

    refactors for semantic code

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,20 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import NewHeader from "./components/NewHeader";
 import Typewriter from "typewriter-effect";
 
 function App() {
   return (
     <div className="App">
-      <NewHeader />
-      <header className="App-header">
+      <header className="App-container">
         <div className="logo-trigger">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <Typewriter
           onInit={(typewriter) => {
             typewriter
-              // .typeString("Hello World!")
               .start()
               .typeString("Edit <code>src/App.js</code>")
               .callFunction(() => {
